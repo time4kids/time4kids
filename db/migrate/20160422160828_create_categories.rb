231c1852fb2fae83925d5f8bf7f3f6db30a3df5d
Solotchi Veaceslav <veaceslav@cashplay.co>
2016-04-22 18:12:45 +0200

    setup class categories model

---
+++ db/migrate/20160422160828_create_categories.rb
@@ -0,0 +1,11 @@
+class CreateCategories < ActiveRecord::Migration
+  def change
+    create_table :categories do |t|
+      t.string :name
+      t.text :description
+      t.references :parent, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
