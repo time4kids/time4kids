4ac0462ad9dc37b3826ee1a3c4de3c1fcbc519ee
Veaceslav Solotchi <veaceslav.solotchi@4finance.com>
2017-01-12 10:47:45 +0100

    add missing fields to user table

---
+++ time4kids-api/db/migrate/20170112094109_add_first_name_and_last_name_to_users.rb
@@ -0,0 +1,6 @@
+class AddFirstNameAndLastNameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string, default: ''
+    add_column :users, :last_name, :string, default: ''
+  end
+end
