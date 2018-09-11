47a844b8256ac3ebc155a3ed7b76c0d900f45fc1
Veaceslav Solotchi <Veaceslav.Solotchi@4finance.com>
2018-09-11 16:34:20 +0200

    some migration adjustments

--- db/migrate/20180909110338_create_school_profile.rb
+++ db/migrate/20180909110338_create_school_profile.rb
@@ -6,10 +6,8 @@ class CreateSchoolProfile < ActiveRecord::Migration[5.2]
       t.string :web_site
       t.text :description
       t.references :address, foreign_key: true
-      t.references :categories, foreign_key: true
     end
 
     add_index :student_profiles, :address
-    add_index :student_profiles, :categories
   end
 end
